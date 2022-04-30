e4ad1900f8ae517a489f013014879b0e41638d7d
xenedium <aabderraziq@gmail.com>
2022-04-30 18:20:57 +0000

    models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=15)
+    picture = models.ImageField(upload_to='profile_pics', blank=True)
+    hashed_password = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.firstname
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    condition = models.CharField(max_length=20)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    quantity = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.title
+
+
+class SoldArticle(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buyer')
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='seller')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.article.title
+
+
+class DeletedArticle(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.article.title

dc940aa25643cc5e7d05d3b644d8c0c3bc7f3344
Ahmed Abderraziq <ahmed@abderraziq.com>
2022-06-28 12:05:45 +0100

    Temporary changes

--- frontend/src/Components/Home/HomeFooter.tsx
+++ frontend/src/Components/Home/HomeFooter.tsx
@@ -136,9 +136,7 @@ export function FooterLinks({ data }: FooterLinksProps) {
                     alt="CatchIt Logo"
                     
                 />
-                    <Text size="xs" color="dimmed" className={classes.description}>
-                        A project by <a href='https://abderraziq.com' style={{color: 'grey'}}>Ahmed Abderraziq</a>, Narjisse Loudiyi, Oussama Tahri & Walid Tika
-                    </Text>
+                    
                 </div>
                 <div className={classes.groups}>{groups}</div>
             </Container>
